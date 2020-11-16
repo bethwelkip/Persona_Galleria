e822eb97709945c2b806e1331882bd18204fb6e6
Bethwel Kiplimo <bkiplimo@princeton.edu>
2020-11-15 19:29:58 -0500

    add view by location

--- gallery/views.py
+++ gallery/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from .models import Image
+from .filldb import initialize
 # Create your views here.
 def profile(request):
     #displays by category
     return render(request, 'category.html')
 
 def index(request):
+    if not Image.pictures():
+        initialize()
     #displays all images
-    return render(request, 'index.html')
+    pics = Image.pictures()
+    print(pics[0].url)
+    return render(request, 'index.html', {"pics": pics})
 
 def location(request):
     #display based on location
-    return render(request, 'location.html')
+    pics = Image.objects.all()
+    print(pics[4].location)
+    return render(request, 'location.html', {"pics": pics})
 
 def search_results(request):
     if 'category' in request.GET and request.GET["category"]:
         search_term = request.GET.get("category")
         searched_images = Image.search_image(search_term)
         message = f"{search_term}"
-        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'search.html',{"message":message,"images": searched_images})
     elif 'location' in request.GET and request.GET["location"]:
         search_term = request.GET.get("location")
         searched_images = Image.search_image(search_term)
         message = f"{search_term}"
-        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
         message = "You haven't searched for any term \n"
