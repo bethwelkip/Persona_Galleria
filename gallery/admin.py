798ad86cfd78b3f15db9b7ddc4156f3d78b079f7
Bethwel Kiplimo <bkiplimo@princeton.edu>
2020-11-17 05:18:44 -0500

    Register categories

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Image, Category, Location
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Category', )
 admin.site.register(Image)
 admin.site.register(Location)
 admin.site.register(Category)
