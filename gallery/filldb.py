2f782646d03e127de6d6f599a2e5e571e60ad440
Bethwel Kiplimo <bkiplimo@princeton.edu>
2020-11-17 08:20:48 -0500

    add directory

--- gallery/filldb.py
+++ gallery/filldb.py
@@ -2,14 +2,14 @@ from .models import Image, Category, Location
 import random
 from random import shuffle
 import cloudinary, cloudinary.api,cloudinary.uploader
-
+import os
 def initialize():
+    cwd = os.getcwd()
+    image = cloudinary.uploader.upload_resource(f'{cwd}/gallery/assets/memes/0.JPG')
     #name, description, location,categories, image
     locations = ["Princeton","Nakuru", "Wawa", "Nairobi", "Beach"]
     categories = ["memes", "nature", "bethwel", "friends"]
-    # shuffle(locations)
-    # shuffle(categories)
-    # meme = f'/assets/memes/{locations[0]}.JPG'
+
     for location in locations:
         loc = Location(location = location)
         loc.save()
@@ -21,31 +21,35 @@ def initialize():
     cats = Category.objects.all()
     for i in range(5):
         img = Image(name ="meme",description= "a favourite meme",location = random.choice(locs))
-        img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/memes/{i}.JPG')
+        img.image = cloudinary.uploader.upload_resource(f'{cwd}/gallery/assets/memes/{i}.JPG')
+        # img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/memes/{i}.JPG')
         img.save()
         img.categories.add(Category.objects.filter(category="memes").first())
         img.save()
     for i in range(9):
         img = Image(name ="walk",description= "a pic of the place I hang out mostly",location = random.choice(locs))
-        img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/nature/{i}.jpg')
+        img.image = cloudinary.uploader.upload_resource(f'{cwd}/gallery/assets/nature/{i}.jpg')
+        # img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/nature/{i}.jpg')
+
         img.save()
         img.categories.add(Category.objects.filter(category="nature").first())
         img.save()
     for i in range(7):
         img = Image(name ="Me",description= "a pic of myself and my friends",location = random.choice(locs))
-        img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/bethwel/{i}.jpg')
+        img.image = cloudinary.uploader.upload_resource(f'{cwd}/gallery/assets/bethwel/{i}.jpg')
+        # img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/bethwel/{i}.jpg')
         img.save()
         img.categories.add(Category.objects.filter(category="bethwel").first())
         img.categories.add(Category.objects.filter(category="friends").first())
         img.save()
     for i in range(7):
         img = Image(name ="Me",description= "a pic of myself and my friends",location = random.choice(locs))
-        img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/fun/{i}.jpg')
+        img.image = cloudinary.uploader.upload_resource(f'{cwd}/gallery/assets/fun/{i}.jpg')
+        # img.image = cloudinary.uploader.upload_resource(f'/Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/fun/{i}.jpg')
         img.save()
         img.categories.add(Category.objects.filter(category="nature").first())
         img.categories.add(Category.objects.filter(category="bethwel").first())
         img.categories.add(Category.objects.filter(category="friends").first())
         img.save()
     pics = [img.name for img in Image.objects.all()]
-    print(pics)
     # /Users/bethwelkiplimo/Desktop/MoringaCore/PersonaGalleria/gallery/assets/
